42100aeda749cf06eb8bbc57db54b2ea216b1b93
Deyyvon <devontbautista@gmail.com>
2018-06-17 22:27:09 -0700

    Removed old structure.

--- arch/basic/.scripts/screenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DUMP="$HOME/screenshots"
-
-scrot $DUMP/%Y-%m-%d_%H:%M:%S.png
-notify-send -a Scrot Screenshot
--- arch/powerline/README.md
+++
@@ -1,2 +0,0 @@
-# powerline: Arch + i3 + i3blocks
-i3WM with Powerline-themed status bar using i3blocks.
