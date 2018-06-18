59fa974bf8936b0b1aedfce85ce435e92c06b930
Deyyvon <devontbautista@gmail.com>
2018-06-17 22:01:04 -0700

    Started using yadm to track cyber theme (put onto own branch).

---
+++ .config/i3/scripts/polybar.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+bar=top
+
+# Terminate already-running bar instances
+killall -q polybar
+
+# Wait until processes have completed shutting down
+while pgrep -x polybar > /dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | sort -r); do
+    MONITOR=$m polybar -l info "$bar" &
+  done
+else
+  # Launch polybar
+  polybar "$bar" &
+fi
