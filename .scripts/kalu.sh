25a1e938185d3fd7d94440e93ae7b8927bcb26b2
Devon Bautista <devontbautista@gmail.com>
2019-01-20 15:16:50 -0700

    Make kalu network check script not start until connected to the Internet (i.e. can ping)

--- .scripts/kalu.sh
+++ .scripts/kalu.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-INTERFACE='wlan0'
-
 # Check if Kalu is installed correctly
 /bin/which kalu 2>&1 > /dev/null
 if [ "$?" -eq 1 ]; then
     exit 1;
 fi
 
-# Are we connected to the internet?
-get_wlan0='ip a | grep "wlan0" | grep -P '"'"'inet(?!6)'"'"
-get_eth0='ip a | grep "eth0" | grep -P '"'"'inet(?!6)'"'"
-wlan0=$(eval "$get_wlan0")
-eth0=$(eval "$get_eth0")
+addr=8.8.8.8
 
-while [[ -z "$wlan0" && -z "$eth0" ]]; do
+# Are we connected to the internet?
+cmd='ping -q -w 1 -c 1 "$addr"'
+eval "$cmd"
+connected=$?
+
+while [ $connected != 0 ]; do
+    eval "$cmd"
+    connected=$?
     sleep 1;
-    wlan0=$(eval "$get_wlan0")
-    eth0=$(eval "$get_eth0")
 done
 
 # Once we are, start Kalu
