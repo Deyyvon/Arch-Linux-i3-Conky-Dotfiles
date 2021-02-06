958a61c9a4b8545c0dcd4f04d27cd7a16eb4b72b
Devon Bautista <devontbautista@gmail.com>
2021-02-05 22:21:27 -0800

    tty: Add color config

---
+++ dotfiles/tty/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0010016
+                 \e]P1C44063
+                 \e]P2E84F65
+                 \e]P3FE6E66
+                 \e]P4FD956A
+                 \e]P5F3D374
+                 \e]P620479B
+                 \e]P796c7d6
+                 \e]P8698b95
+                 \e]P9C44063
+                 \e]PAE84F65
+                 \e]PBFE6E66
+                 \e]PCFD956A
+                 \e]PDF3D374
+                 \e]PE20479B
+                 \e]PF96c7d6
+                 \ec'
